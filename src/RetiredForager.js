344a49b5aa952d941b46f102c8f10752330fc0f5
junma <junma0704@gmail.com>
2021-09-27 11:34:59 -0700

    retiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this, arguments);
+  this. age = 40;
+  this.job = 'gamble';
+  this.color = 'grey';
+  this.canFly = false;
+
+};
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.eat = function() {
+  Grub.eat.call(this, arguments);
+};
+RetiredForagerBee.prototype.gamble = function() {
+  // this.treasureChest.push('treasure');
+  foragerBee.forage.call(this, arguments);
 };
 
