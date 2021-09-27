18d685d011953d78b09cf43efb3b42e75a1e0112
junma <junma0704@gmail.com>
2021-09-27 11:29:23 -0700

    create Foragerbee class'

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  Bee.call(this, arguments);
+  this. age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
+};
+ForagerBee.prototype.eat = function() {
+  Grub.eat.call(this, arguments);
+};
+ForagerBee.prototype.forage = function() {
+  this.treasureChest.push('treasure');
 };
 
