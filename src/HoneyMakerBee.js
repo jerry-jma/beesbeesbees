1dc0d815d51098216f9f243e217744107329634e
junma <junma0704@gmail.com>
2021-09-27 11:22:24 -0700

    create HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this, arguments);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype.eat = function() {
+  Grub.eat.call(this, arguments);
+};
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
